14aa2a72f4d101463f9aef46b886bacd57aacb44
Pablo Bernardo @voylinux <voylinux@gmail.com>
2016-03-05 03:27:42 +0100

    Here is where my new web beggins

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # voylinux.com-frontend
 Design for a node app as my new web frontend
+
+This is an under development design for a :heart_eyes:
+new personal web page.
+
+Te idea is having a JS app that will include my blog using WordPress API REST
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+module.exports = {
+  entry: {
+    src: ["./src/entry.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "src/sass")]
+  }
+}
