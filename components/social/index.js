226ced2404fc0fa4ca3e0c2da2d30c8e3acc122f
voylinux <voylinux@gmail.com>
2016-03-09 02:05:38 +0100

    typo in meta tag viewport and fixes in social media queries

--- components/social/index.js
+++ components/social/index.js
@@ -6,6 +6,7 @@ class Social extends React.Component {
       <ul className="social-list">
         <li><a href="#"><i className="fa fa-github"></i></a></li>
         <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i className="fa fa-twitter"></i></a></li>
         <li><a href="#"><i className="fa fa-instagram"></i></a></li>
         <li><a href="#"><i className="fa fa-spotify"></i></a></li>
       </ul>
